8ed9a199996de6832e1e8dcff13b1307cd1408e0
Bryan Ndjeutcha <ndjeutcha@gmail.com>
2026-01-06 08:52:17 -0500

    Make second PoW mask byte 0xFF

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // A mask requiring ~2.5 bytes of zeros for a valid PoW solution.
     let mut mask = [0_u8; 32];
     mask[0] = 0xFF;
-    mask[1] = 0xF0;
+    mask[1] = 0xFF;
     // mask[2] = 0xF0; // 20 bits of zeros
 
     let coinbase_tx = Transaction {
